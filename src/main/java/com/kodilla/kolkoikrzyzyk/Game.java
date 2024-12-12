31fa3b9ce7f0b84f1a4b0e74dbbbfce56685517b
Roman <roman.adamczyk12@gmail.com>
2024-12-12 15:52:32 +0100

    9.2 Projekt indywidualny - first commit

---
+++ src/main/java/com/kodilla/kolkoikrzyzyk/Game.java
@@ -0,0 +1,4 @@
+package com.kodilla.kolkoikrzyzyk;
+
+public class Game {
+}
---
+++ src/test/java/com/kodilla/kolkoikrzyzyk/KolkoIKrzyzykApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kolkoikrzyzyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolkoIKrzyzykApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
