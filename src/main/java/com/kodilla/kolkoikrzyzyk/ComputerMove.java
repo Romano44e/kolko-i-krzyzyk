324d4fffc36224f53b446a08d15d226423e7cf39
Roman <roman.adamczyk12@gmail.com>
2024-12-16 19:50:10 +0100

    modules 9-10 - individual project - corrections in ComputerMove class

--- src/main/java/com/kodilla/kolkoikrzyzyk/ComputerMove.java
+++ src/main/java/com/kodilla/kolkoikrzyzyk/ComputerMove.java
@@ -13,30 +13,30 @@ public class ComputerMove {
             int column = random.nextInt(3) + 1;
             int row = random.nextInt(3) + 1;
 
-            Integer[] ComputerPlayerMoveBoard = new Integer[2];
-            ComputerPlayerMoveBoard[0] = column;
-            ComputerPlayerMoveBoard[1] = row;
+            Integer[] computerPlayerMoveBoard = new Integer[2];
+            computerPlayerMoveBoard[0] = column;
+            computerPlayerMoveBoard[1] = row;
 
             if (board[row - 1][column - 1].contains("X")
                     || board[row - 1][column - 1].contains("O")) {
                 return getComputerPlayerMove(board);
             }
-            return ComputerPlayerMoveBoard;
+            return computerPlayerMoveBoard;
         }
 
         if (board.length == 10) {
             int column = random.nextInt(10) + 1;
             int row = random.nextInt(10) + 1;
 
-            Integer[] ComputerPlayerMoveBoard = new Integer[2];
-            ComputerPlayerMoveBoard[0] = column;
-            ComputerPlayerMoveBoard[1] = row;
+            Integer[] computerPlayerMoveBoard = new Integer[2];
+            computerPlayerMoveBoard[0] = column;
+            computerPlayerMoveBoard[1] = row;
 
             if (board[row - 1][column - 1].contains("X")
                     || board[row - 1][column - 1].contains("O")) {
                 return getComputerPlayerMove(board);
             }
-            return ComputerPlayerMoveBoard;
+            return computerPlayerMoveBoard;
         }
         return null;
     }
